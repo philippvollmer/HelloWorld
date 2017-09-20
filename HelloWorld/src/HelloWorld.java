fc6f8b31fa99c2ca24b545692d7c94b24ba1825c
Vollmer.Philipp <Vollmer.Philipp@SK-448A5BCB8A89.2631.skool.net>
2017-09-20 16:54:31 +0200

    Collatz Folge Fertig

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,17 +3,24 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int n = 19;
 		
+int n;
+for(int i=1;i<=1000000000;i++){
+	n=i;
+System.out.print(n+": ");
+	while(n>1){	
 if(n%2==0){
 	n=n/2;
 }else{
 	n=3*n+1;
 }
-	System.out.println(n);
+	System.out.print(n+" ");
+	
+	}
+	System.out.println("");
+	}
+	}
 	}
 
 
 
-
-}
